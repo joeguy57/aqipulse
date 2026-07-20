467558a4d090c0dda1044858ca4937b1d35c96c3
joeguy57 <josephm340@gmail.com>
2026-07-19 20:25:37 -0600

    feat: connected databricks

--- python/02_validate.py
+++ python/02_validate.py
@@ -5,3 +5,15 @@ Connects to Databricks and runs validation queries on the bronze table.
 Confirms data loaded correctly before moving to the silver transformation
 """
 
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from databricks import sql as dbsql
+
+load_dotenv()
+
+conn = dbsql.connect(
+    server_hostname= os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+    http_path= os.getenv("DATABRICKS_HTTP_PATH"),
+    access_token= os.getenv("DATABRICKS_ACCESS_TOKEN"),
+)
